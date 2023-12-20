31378a49fd4b2ec458b3efb1e90d0999b2b33526
chrimcha <147195321+chrimcha@users.noreply.github.com>
2023-12-20 14:37:50 -0600

    Read and completed tasks in file, missing package-lock.json file for some reason.

--- hello.js
+++ hello.js
@@ -4,19 +4,27 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log ("Hello, World!");
+console.log ("Welcome to Turtle Town,");
+console.log ("Population 1M.");
+console.log ("Pear are good.", "So are oranges.");
+console.log ("'Nevermore'");
+console.log ('"Nevermore"');
+console.log ("Complete!")
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
-   1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
-   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
-   5. Print a number. (Bonus: Print two numbers added together).
-   6. Print multiple messages one after the other.
-   7. Print two messages on the same line.
-   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
-   9. Other. You choose!
+   1. Change the message that is printed. (Done)
+   2. Figure out what the parentheses do. Will the code work without them? (No)
+   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? (Yes)
+      Is there a difference between using a single or a double quote (' vs. ")? (In this case no difference)
+   4. Remove the semi-colon, ;. (Runs without (;))
+   5. Print a number. (Bonus: Print two numbers added together). 
+      (With "" is a string and will print "1+1" if "" are used, without "" "1+1" will print at "2")
+   6. Print multiple messages one after the other. (Done)
+   7. Print two messages on the same line. (Done)
+   8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore". (Done)
+   9. Other. You choose! (Done)
 */
